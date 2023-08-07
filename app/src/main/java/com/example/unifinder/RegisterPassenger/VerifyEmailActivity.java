fdf228554e01f0292aa63670c6e9a9a9477d10ff
Abdul Qadeer <112065732+Mrqadeer09@users.noreply.github.com>
2023-08-07 19:49:21 +0500

    changessar

--- app/src/main/java/com/example/unifinder/RegisterPassenger/VerifyEmailActivity.java
+++ app/src/main/java/com/example/unifinder/RegisterPassenger/VerifyEmailActivity.java
@@ -1,55 +1,28 @@
 package com.example.unifinder.RegisterPassenger;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.unifinder.MainActivity;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.example.unifinder.R;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 public class VerifyEmailActivity extends AppCompatActivity {
 
     TextView btnLogin;
-
-//    private void showLongPressDialog(Context context) {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-//        builder.setTitle("Do you want to move this file").setMessage("You have long-pressed the view!").setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.dismiss();
-//            }
-//        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.dismiss();
-//            }
-//        });
-//
-//        AlertDialog dialog = builder.create();
-//        dialog.show();
-//    }
 
 
     private void showLongPressDialog(Context context) {
@@ -95,22 +68,9 @@ public class VerifyEmailActivity extends AppCompatActivity {
         dialog.show();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    public void buttonOpenFile(View view) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
-//        intent.setType("application/pdf");
-        intent.setType("*/*");
-        this.startActivity(intent);
-    }
 
 
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    public void buttonCreateFile(View view) {
-        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
-//        intent.setType("application/pdf");
-        intent.setType("*/*");
-        this.startActivity(intent);
-    }
+
 
 
 
@@ -119,17 +79,6 @@ public class VerifyEmailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_email);
         findViews();
-
-
-
-
-
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.READ_EXTERNAL_STORAGE},
-                PackageManager.PERMISSION_GRANTED);
-
-
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
